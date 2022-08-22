57334ab19b3557143055591eee3149d83e1a188d
Mateus Danielle <mateusdanielle@edu.fecap.br>
2022-08-22 16:44:34 -0300

    📏  Adding Header Component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import styles from './styles.module.scss';
+
+export function Header() {
+  return (
+    <header className={styles.headerComponent}>
+      {/* <div>
+        <h1>Products</h1>
+      </div> */}
+      <h1>Products</h1>
+      {/* <h1>Products</h1>
+      <h1>Products</h1> */}
+    </header>
+  )
+}

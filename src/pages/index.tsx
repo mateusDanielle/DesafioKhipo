fe76ea691d58be100d67b7c8fec3633d7a686b58
Mateus Danielle <mateusdanielle@edu.fecap.br>
2022-08-22 16:00:33 -0300

    🎨  Adding SASS/Custom Font/Global Styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,15 @@
+import Head from 'next/head';
+
+import styles from './home.module.scss';
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <>
+      {/* Desta forma é possível ter um título dinâmico para cada página */}
+      <Head>
+        <title>Produtos | Desafio Khipo</title>
+      </Head>
+      <h1 className={styles.title}>Hello World</h1>
+    </>
   )
 }

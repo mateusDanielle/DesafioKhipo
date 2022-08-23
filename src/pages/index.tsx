54c08b00d450fa53460bfb28153da87625bfd6db
Mateus Danielle <mateusdanielle@edu.fecap.br>
2022-08-23 14:40:49 -0300

    🧠  Adding Swr and Initial Request

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,26 @@
 import Head from 'next/head';
 
+import useSwr from 'swr'
+
+import { Product } from '../components/Product/indext';
+
 import styles from './home.module.scss';
 
+const fetcher = (url: string) => fetch(url).then((res) => res.json())
+
 export default function Home() {
+  const { data, error } = useSwr<any>('/api/products', fetcher)
+
+  if (error) return <div>Erro ao ler usuários</div>
+  if (!data) return <div>Carregando...</div>
+
   return (
     <>
       {/* Desta forma é possível ter um título dinâmico para cada página */}
       <Head>
         <title>Produtos | Desafio Khipo</title>
       </Head>
-      <h1 className={styles.title}>Hello World</h1>
+      <Product />
     </>
   )
 }

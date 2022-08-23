be670035b8c40570ff01911895ba6c7e399ac450
Mateus Danielle <mateusdanielle@edu.fecap.br>
2022-08-23 15:08:43 -0300

    🕵️  Await had fled

--- src/pages/api/products/[id].ts
+++ src/pages/api/products/[id].ts
@@ -5,20 +5,20 @@ import { getProduct, updateProduct, deleteProduct } from '../../../services/pris
 export default async (request: NextApiRequest, response: NextApiResponse) => {
   const { id } = request.query;
   if (request.method === 'GET') {
-    const product = getProduct(id);
+    const product = await getProduct(id);
 
     return response.status(200).json({ product });
   };
 
   if (request.method === 'PATCH') {
     const { name, price, brand, image } = request.body;
-    const product = updateProduct(id, name, price, brand, image);
+    const product = await updateProduct(id, name, price, brand, image);
 
     return response.status(200).json({ product });
   };
 
   if (request.method === 'DELETE') {
-    const req = deleteProduct(id);
+    const req = await deleteProduct(id);
 
     return response.status(200).json({ req });
   };
--- src/pages/api/products/index.ts
+++ src/pages/api/products/index.ts
@@ -4,13 +4,13 @@ import { getProducts, postProduct } from '../../../services/prisma'
 
 export default async (request: NextApiRequest, response: NextApiResponse) => {
   if (request.method === 'GET') {
-    const products = getProducts();
+    const products = await getProducts();
 
     return response.status(200).json({ products });
   };
   if (request.method === 'POST') {
     const { name, price, brand, image } = request.body;
-    const product = postProduct(name, price, brand, image);
+    const product = await postProduct(name, price, brand, image);
 
     return response.status(201).json({ product });
   };

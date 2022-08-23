82e2ecdad29df8b0692dd1d88c6a53e9f3a38e15
Mateus Danielle <mateusdanielle@edu.fecap.br>
2022-08-23 15:01:30 -0300

    🛍  Finishing product component

--- src/components/Product/indext.tsx
+++ src/components/Product/indext.tsx
@@ -1,13 +1,21 @@
 import styles from './styles.module.scss';
 
-export function Product() {
+interface Props {
+  id: string, 
+  image: string,
+  name: string,
+  brand: string,
+  price: string,
+}
+
+export function Product({ id, image, name, brand, price }: Props) {
   return (
     <div className={styles.productComponent}>
     <div className={styles.container}>
-      <img src="http://loremflickr.com/640/480/fashion" alt="Product id 1"/>
-      <p>Product Name 1</p>
-      <p>Brand Name 1</p>
-      <h1>R$ 765.00</h1>
+      <img src={image} alt={`Product id ${id}`}/>
+      <p>{name}</p>
+      <p>{brand}</p>
+      <h2>R$ {price}</h2>
       </div>
     </div>
   )
